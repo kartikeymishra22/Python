baea1caedf2a2303f1877e8c308b85bd9525fbde
Kartikey Mishra <kartikeymishra700@gmail.com>
2024-10-28 23:09:39 +0530

    first commit

---
+++ Day 11/main.py
@@ -0,0 +1,14 @@
+# name = "Kartikey"
+# print("Hello, " + name)
+
+# name = "Harry"
+# print(name[-4:-2])
+
+# str1 = "    Silver Spoon       "
+# print(str1.strip())
+
+
+str2 = "Silver Spoon"
+print(str2.replace("Sp", "M"))
+
+print(str2)
---
+++ Day 20/main.py
@@ -0,0 +1,10 @@
+def calculateMean(a,b):
+    mean = (a*b)/(a+b)
+
+    print(mean)
+
+
+a = 10
+b = 9
+
+calculateMean(a,b)
