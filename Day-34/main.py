8b2a95b610294c2a827d20e3f1a1124ea4bbd592
Kartikey Mishra <kartikeymishra700@gmail.com>
2025-08-06 15:39:24 +0530

    updation

--- Day-34/main.py
+++ Day-34/main.py
@@ -1,8 +1,24 @@
-dict = {
-    "name": "Kartik",
-    "age" :20,
-}
+# dict = {
+#     "name": "Kartik",
+#     "age" :20,
+# }
 
-print(dict)
-dict.update({"address":"Gurgaon"})
-print(dict)
+# print(dict)
+# dict.update({"address":"Gurgaon"})
+# print(dict)
+
+# c = [10,20,23]
+# y = c
+# y[1] = 30
+# print(c)
+
+# This code snippet is creating a list `li` with elements 'python', 'java', and 'php'.
+# li = ['python','java','php']
+# print(li[1][-1])
+# print(len(li))
+li = [1,2,3,4]
+for num in li:
+    if num ==3:
+        break
+else:
+    print("No else")     
